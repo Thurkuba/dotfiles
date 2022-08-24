3660f03339f2677eaaf9597567ddcc46608bc2f4
thurkuba <arthur.kubagawa@gmail.com>
2022-08-23 23:26:59 -0300

    dotfiles init

---
+++ .config/fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+zoxide init fish | source
+
+starship init fish | source
+
+set --universal EDITOR /bin/vim 
+
+
+alias config='/usr/bin/git --git-dir=$HOME/.kconfig/ --work-tree=$HOME'
